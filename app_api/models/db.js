456cf652ac2c10284a5129797372455dab5bc0e8
Furkan Burak Öztürk <furkanburakozturk1@gmail.com>
2025-11-27 14:48:17 +0300

    first commit

---
+++ app_api/models/db.js
@@ -0,0 +1,20 @@
+var mongoose=require('mongoose');
+var dbURI= 'mongodb+srv://test:test@cluster0.mlkjz0b.mongodb.net/mekanbul?retryWrites=true&w=majority&appName=Cluster0';
+//'mongodb://localhost/mekanbul'
+//mongodb+srv://test:<db_password>@cluster0.mlkjz0b.mongodb.net/?appName=Cluster0
+mongoose.connect(dbURI);
+mongoose.connection.on('connected',function(){
+    console.log(dbURI+' adresindeki veritabanına bağlanıldı.');
+});
+mongoose.connection.on('error',function(err){
+    console.log('Veritabanı bağlantı hatası: '+err);
+});
+mongoose.connection.on('disconnected',function(){
+    console.log('Bağlantı kesildi.');
+});
+process.on("SIGINT", function(){
+    mongoose.connection.close();
+    console.log("Uygulama sonlandırıldı, bağlantı kapatıldı.");
+    process.exit(0);
+});
+require('./venue'); 
